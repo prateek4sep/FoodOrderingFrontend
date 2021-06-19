c781f727a1476f9b939ef9f2e15e3d3d80e85903
sharmabhushan1994@gmail.com <sharmabhushan1994@gmail.com>
2021-06-19 20:24:00 +0530

    Profile Page Added

---
+++ src/screens/profile/Profile.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import "./Profile.css";
+import Header from "../../common/header/Header";
+
+class Profile extends Component {
+  render() {
+    if (!localStorage.getItem("access-token")) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <div>
+        <Header history={this.props.history} />
+        <div className="profile-container">Profile Page</div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
